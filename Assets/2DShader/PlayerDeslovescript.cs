5c01287b40c4101839e3f425b66880aca8c6eb09
AniketKumbhar11 <atkumbhar1@gmail.com>
2021-04-27 11:25:29 +0530

    first commit

---
+++ Assets/2DShader/PlayerDeslovescript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeslovescript : MonoBehaviour
+{
+    Material material;
+    bool isDissolving = false;
+    float fade = 1f;
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isDissolving)
+            {
+                isDissolving = false;
+            } else
+            {
+                isDissolving = true;
+            }
+
+        }
+
+
+
+
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+            if (fade<=0)
+            {
+                fade = 0f;
+                isDissolving = false;
+            }
+            material.SetFloat("_fade", fade);
+        }
+        else
+        {
+            fade += Time.deltaTime;
+            if (fade >= 1)
+            {
+                fade = 1f;
+                isDissolving = true;
+            }
+            material.SetFloat("_fade", fade);
+        }
+    }
+}
